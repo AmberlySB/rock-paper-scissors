0b50a7a9496f9f98a3b1ed8dd4d23e042984928b
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-11 16:38:50 -0500

    Add prompt to get user's choice

---
+++ script.js
@@ -0,0 +1,13 @@
+// Ask user for their choice
+let userChoice = prompt ("Please choose one: Rock, Paper, or Scissors", "")
+console.log(userChoice);
+
+//get computer choice
+
+//compare user choice to computer choice
+
+//declare winner based on comparison result
+
+//make variable to store results to keep score
+
+//repeat process until five rounds have been played
