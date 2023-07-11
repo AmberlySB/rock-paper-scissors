18bf8538792fe4a7836fddf0eb042727f7b3ba8e
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-11 17:19:27 -0500

    Add function to get computer's choice

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ let userChoice = prompt ("Please choose one: Rock, Paper, or Scissors", "")
 console.log(userChoice);
 
 //get computer choice
+function getComputerChoice (){
+    let randomNum = Math.floor(Math.random() * 90) + 1;
+    let computerChoice;
+    if (randomNum < 31) {
+        computerChoice = "rock";
+    }
+    else if (randomNum > 60) {
+        computerChoice = "scissors";
+    }
+    else {
+        computerChoice = "paper";
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
 
 //compare user choice to computer choice
 
