6b2ff93d101e4abe5b7d57e50e5c2ab82872d46b
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-11 19:43:04 -0500

    Add function to play a single round of rock, paper, scissors. Convert player choice prompt to function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Ask user for their choice
-let userChoice = prompt ("Please choose one: Rock, Paper, or Scissors", "")
-console.log(userChoice);
+// Ask player for their choice
+function getPlayerChoice (){
+    let playerChoice = prompt ("Please choose one: Rock, Paper, or Scissors", "")
+    console.log(playerChoice);
+    return playerChoice;
+}
 
 //get computer choice
 function getComputerChoice (){
@@ -15,14 +18,51 @@ function getComputerChoice (){
     else {
         computerChoice = "paper";
     }
+    console.log(computerChoice);
     return computerChoice;
 }
 
-console.log(getComputerChoice());
-
 //compare user choice to computer choice
-
 //declare winner based on comparison result
+function playRound (playerSelection, computerSelection) {
+    //console.log(computerChoice);
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock!";
+        }
+        else if (computerSelection === "scissors") {
+            return "You win! Rock beats Scissors!";
+        }
+        else {
+            return "It's a draw!";
+        }
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beats Paper!";
+        }
+        else if (computerSelection === "rock") {
+            return "You win! Paper beats Rock!";
+        }
+        else {
+            return "It's a draw!";
+        }
+    }
+    else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors!";
+        }
+        else if (computerSelection === "paper") {
+            return "You win! Scissors beats Paper!";
+        }
+        else {
+            return "It's a draw!";
+        }
+    }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
+
 
 //make variable to store results to keep score
 
